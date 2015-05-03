53499765a8b82e53e2fd7dc229f757085e176e52
Alex Svtwola <alex.cartagena@hotmail.com>
2015-05-03 14:50:51 +0200

    Added GPL License

--- PKeyConfigReader/VSSharedSource.vb
+++ PKeyConfigReader/VSSharedSource.vb
@@ -1,4 +1,28 @@
-﻿Public Class VSSharedSource
+'============================================================================
+'
+'    PKeyConfigReader
+'    Copyright (C) 2013 - 2015 Visual Software Corporation
+'
+'    Author: ASV93
+'    File: VSSharedSource.vb
+'
+'    This program is free software; you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation; either version 2 of the License, or
+'    (at your option) any later version.
+'
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License along
+'    with this program; if not, write to the Free Software Foundation, Inc.,
+'    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'
+'============================================================================
+
+Public Class VSSharedSource
     Dim donationpage As String = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=XTCNPRUDFH4UA&lc=ES&item_name=PKEYCONFIG&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted"
     Dim appname As String = "PKEYCONFIG"
 
